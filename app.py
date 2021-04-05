0cab82390127ca75ee7828c53b04b20544b40f9b
wileczkam <wileczkam@ainfosec.com>
2021-04-05 10:07:45 -0400

    Initial Test Commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, leave_room, emit, Namespace
+from flask_session import Session
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)
+
+socketio = SocketIO(app, manage_session=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    if request.method == 'POST':
+        username = request.form['username']
+        room = request.form['room']
+        # Store the data in session
+        session['username'] = username
+        session['room'] = room
+        return render_template('chat.html', session=session)
+    else:
+        if session.get('username') is not None:
+            return render_template('chat.html', session=session)
+        else:
+            return redirect(url_for('index'))
+
+
+class ChatNamespace(Namespace):
+    def on_join(self, message):
+        room = session.get('room')
+        join_room(room)
+        emit('status', {'msg': session.get('username') + ' has entered the room.'}, room=room)
+
+    def on_text(self, message):
+        room = session.get('room')
+        # send this to entire room
+        emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+        # send this back only to b
+        emit('message', {'msg': 'you sent ' + message['msg'].upper() })
+
+    def on_left(self, message):
+        room = session.get('room')
+        username = session.get('username')
+        leave_room(room)
+        session.clear()
+        emit('status', {'msg': username + ' has left the room.'}, room=room)
+
+    def on_disconnect(self):
+        room = session.get('room')
+        username = session.get('username')
+        print('Client disconnected', room, username)
+
+    def on_connect(self):
+        room = session.get('room')
+        username = session.get('username')
+        print('Client connect', room, username)
+
+    def on_play(self, message):
+        room = session.get('room')
+        username = session.get('username')
+        print('Played', message)
+
+
+socketio.on_namespace(ChatNamespace('/chat'))
+
+if __name__ == '__main__':
+    socketio.run(app)
---
+++ doc/protocol.md
@@ -0,0 +1,175 @@
+SEATS = 0,1,2,3
+TEAMS = 0,1
+CARDS = 2H,3S,4D,5C
+
+
+
+```mermaid
+stateDiagram-v2
+  [*] --> ReqBet: Deal
+  ReqBet --> ReqBet: Bet
+  ReqBet --> ReqSuit: Won Bet
+  ReqSuit --> ReqDiscard: Suit
+  ReqDiscard --> ReqPlay: Discard
+  ReqPlay --> ReqPlay: Play
+  ReqPlay --> Trick: Play (last)
+  Trick --> ReqPlay: delay
+  Trick --> EndOfHand: delay (last)
+  EndOfHand --> [*]: delay or all ack
+      
+```
+
+
+
+
+
+
+# Server to Client
+
+## Hand Update
+
+Sent whenever cards are dealt, played, discarded, etc.
+Cards will be pre-sorted
+
+```
+hand:{
+  cards: [str (card), ...]
+}
+```
+
+## Table Update
+
+Sent after every action
+
+```
+table:{
+  trump: str (suit)
+  dealer: int (0-3)       (or use seat flag)
+  lead: int (0-3)       (or use seat flag)
+  seats:[
+    {
+      hand: int (0-10)
+      played: str (card)
+      bid: 0-19
+      dealer: bool        (or use index)
+      lead: bool          (or use index)
+    },
+    ...
+  ]
+  score: [int, int]
+  points: [int, int]
+}
+```
+
+## Log
+
+Sent as a game log for who played what and took tricks, score changes, etc.
+Show in a scrolling text box.
+
+```
+log:{
+  msg: str
+}
+```
+
+## Dialog
+
+Alert user to problem with ok button (played invalid card, etc.)
+
+```
+dialog:{
+  msg: str
+}
+```
+
+## Request Bid
+
+```
+req_bid:{
+  min: int, 1-18
+}
+```
+
+## Request Suit
+```
+req_suit:{
+  suit: str (suit)
+}
+```
+
+## Request Discard
+Only sent if >6 trump cards after taking kitty
+```
+req_discard:{
+  count: int, 1-10
+}
+```
+
+## Request Play
+```
+req_play:{
+  lead: str (suit)
+  trump: str (suit)
+}
+```
+
+## End of Trick
+NOTE: not sure we need this
+```
+trick:{
+  ???
+}
+```
+
+## Request Deal (End of Hand)
+
+Maybe show dialog with hand results, ask for OK to deal (continue)
+
+```
+req_deal:{
+  ???
+}
+```
+
+# Client to Server
+
+## Sit
+```
+sit:{
+  seat: int, 0-3, or null for no seat (stand up)
+}
+```
+
+## Bid
+```
+bid:{
+  bid: int, 0=pass, 1-18, 19=moon 
+}
+```
+
+## Suit
+```
+suit:{
+  suit: str (suit)
+}
+```
+
+## Discard
+```
+discard:{
+  cards: array of str (card)
+}
+```
+
+## Play
+```
+play:{
+  card: str (card)
+}
+```
+
+## Deal
+
+```
+deal:{}
+```
