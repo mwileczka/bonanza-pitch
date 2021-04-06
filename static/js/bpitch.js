5de685a3bb454b6b14d9d6aeedf7b9e0d7944c3e
lwileczka <laurawileczka@gmail.com>
2021-04-06 00:44:26 -0400

    css card containers

--- static/js/bpitch.js
+++ static/js/bpitch.js
@@ -6,17 +6,34 @@
             AddCardToHand('10S', 'divHand');
             AddCardToHand('5D', 'divHand');
 
-            AddCardToHand('RED_BACK', 'divHandTop');
-            AddCardToHand('RED_BACK', 'divHandTop');
-            AddCardToHand('RED_BACK', 'divHandTop');
-            AddCardToHand('RED_BACK', 'divHandTop');
-            AddCardToHand('RED_BACK', 'divHandTop');
-            AddCardToHand('RED_BACK', 'divHandTop');
+            var i;
+            for (i = 0 ; i < 6 ; i++){
+                AddCardToHand('RED_BACK', 'divHandTop');
+            }
+            for (i = 0 ; i < 6 ; i++){
+                AddCardToHand('RED_BACK', 'divHandLeft');
+            }
+            for (i = 0 ; i < 6 ; i++){
+                AddCardToHand('RED_BACK', 'divHandRight');
+            }
+
+            SetPlayCard("3C", "playedCardTop");
+            SetPlayCard("4C", "playedCardBottom");
+             SetPlayCard("5C", "playedCardLeft");
+             SetPlayCard("6C", "playedCardRight");
         }
 function AddCardToHand(card, hand){
     var container = document.getElementById(hand);
     var img = document.createElement("img");
     img.setAttribute("src", "../static/cards/" + card + ".svg")
     img.setAttribute("class", "pcard")
+    container.appendChild(img);
+}
+function SetPlayCard(card, elem){
+    var container = document.getElementById(elem);
+    var img = document.createElement("img");
+    img.setAttribute("src", "../static/cards/" + card + ".svg")
+    img.setAttribute("class", "pcard")
+    container.innerHTML = "";
     container.appendChild(img);
 }
