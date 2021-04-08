886a97af277a47588b2634be4bdf944ec3548635
Mike Wileczka <mikewileczka@gmail.com>
2021-04-08 02:49:22 -0400

    More renaming. Table join and leave working.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_socketio import SocketIO, join_room, leave_room, emit, Namespace, roo
 import flask_socketio
 from flask_session import Session
 from pitch import Table, Deck
+from threading import Lock
 import flask
 
 app = Flask(__name__)
@@ -13,9 +14,12 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 Session(app)
 
-socketio = SocketIO(app, manage_session=False)
+socketio = SocketIO(app, manage_session=False, async_mode="threading")
 
 tables = {}  # type: Dict[Table]
+
+thread = None
+thread_lock = Lock()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,12 +31,13 @@ def index():
 def table():
     if request.method == 'POST':
         username = request.form['username']
-        room = request.form['room']
+        table = request.form['table']
         seat = request.form['seat']
         # Store the data in session
         session['username'] = username
-        session['room'] = room
+        session['table'] = table
         session['seat'] = seat
+        # TODO add check for avail seat here
         return render_template('table.html', session=session)
     else:
         if session.get('username') is not None:
@@ -43,62 +48,87 @@ def table():
 
 class PitchNamespace(Namespace):
     def on_join(self, message):
-        room = session.get('room')
+        table_name = session.get('table')
         username = session.get('username')
-        request.sid
-        join_room(room)
-        print("join", room, username, request.sid)
+        seat = session.get('seat')
+        join_room(table_name)
+        print("join", username, table_name, seat)
 
-        sio = flask.current_app.extensions['socketio']
-        print(sio.server.manager.rooms)
+        # sio = flask.current_app.extensions['socketio']
+        # print(sio.server.manager.rooms)
 
-        emit('status', {'msg': username + ' has entered the room.'}, room=room)
-        t = tables.setdefault(room, Table(id=room, tx=self.tx))
+        emit('status', {'msg': username + ' has joined the table.'}, room=table_name)
+        t = tables.setdefault(table_name, Table(id=table_name, tx=self.tx))
 
     def on_text(self, message):
-        room = session.get('room')
-        # send this to entire room
-        emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
-        # send this back only to b
-        emit('message', {'msg': 'you sent ' + message['msg'].upper()})
-
-    def on_left(self, message):
-        room = session.get('room')
+        table_name = session.get('table')
         username = session.get('username')
-        print('left', room, username, request.sid)
-        leave_room(room)
-        session.clear()
+        seat = session.get('seat')
 
-        emit('status', {'msg': username + ' has left the room.'}, room=room)
+        # send this to entire room
+        emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=table_name)
+
+    def on_leave(self, message):
+        table_name = session.get('table')
+        username = session.get('username')
+        seat = session.get('seat')
+        print('left', username, table_name, seat)
+        leave_room(table_name)
+        del session['table']
+        del session['seat']
+
+        emit('status', {'msg': username + ' has left the table.'}, room=table_name)
 
     def on_disconnect(self):
-        room = session.get('room')
+        table_name = session.get('table')
         username = session.get('username')
-        print('disconnect', room, username, request.sid)
+        seat = session.get('seat')
+        print('disconnect', username, table_name, seat)
+        emit('status', {'msg': username + ' has been disconnected.'}, room=table_name)
 
     def on_connect(self):
-        room = session.get('room')
+        table_name = session.get('table')
         username = session.get('username')
-        print('connect', room, username, request.sid)
+        seat = session.get('seat')
+        print('connect', username, table_name, seat)
+        emit('status', {'msg': username + ' has been connected.'}, room=table_name)
+        session['sid'] = request.sid
 
-        t = tables.setdefault(room, Table(id=room, tx=self.tx))
+        global thread
+        with thread_lock:
+            if thread is None:
+                thread = socketio.start_background_task(self.background_thread)
+
+        t = tables.setdefault(table_name, Table(id=table_name, tx=self.tx))
+
+    def background_thread(self):
+        print("Background thread started")
+        count = 0
+        while True:
+            socketio.sleep(5)
+            print("Background triggered")
+            count += 1
+            socketio.emit('status', {'msg': f'count is {count}'}, namespace=self.namespace)
 
     def on_play(self, message):
-        room = session.get('room')
+        table_name = session.get('table')
         username = session.get('username')
+        seat = session.get('seat')
         print('Played', message)
 
     def on_update(self):
-        room = session.get('room')
+        table_name = session.get('table')
         username = session.get('username')
-        t = tables[room]
+        seat = session.get('seat')
+        t = tables[table_name]
         emit('table', t.get_json())
         emit('hand', t.get_seat(username).get_hand_json())
 
     def on_deal(self):
-        room = session.get('room')
+        table_name = session.get('table')
         username = session.get('username')
-        t = tables[room]
+        seat = session.get('seat')
+        t = tables[table_name]
         t.deal()
         emit('table', t.get_json())
         emit('hand', t.get_seat(username).get_hand_json())
@@ -110,6 +140,7 @@ class PitchNamespace(Namespace):
     def on_req_lobby(self):
         print('on_req_lobby')
         emit('lobby', [{'name': 'Matrix', 'seats': [None, 'Laura', 'Mike', None]}])
+
 
 socketio.on_namespace(PitchNamespace('/pitch'))
 
