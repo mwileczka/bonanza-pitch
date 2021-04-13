d57492df585283f3d7fc2ca213c01f7f6ab1bd1a
lwileczka <laurawileczka@gmail.com>
2021-04-13 10:01:36 -0400

    Display fixes. User can only play from approved cards.

--- static/js/bpitch.js
+++ static/js/bpitch.js
@@ -1,18 +1,12 @@
-var tableState = {theBid: 0, trump:'', dealer:0, lead:0,
-    seats:[
-        {hand:6, played:'',bid:-1,dealer:true,lead:false,username:'Laura'},
-        {hand:6, played:'',bid:-1,dealer:false,lead:false,username:'Teddy'},
-        {hand:6, played:'',bid:-1,dealer:false,lead:false,username:'Ben'},
-        {hand:6, played:'',bid:-1,dealer:false,lead:false,username:'Mike'}
-    ]
-    , score:[0,0], points:[0,0], deck_cnt:0, kitty_cnt:0};
+var tableState = {};
 
 function StartGame(){
     //RefreshTable(tableState);
 //OnRequestBid(0);
-   // OnRequestSuit();
+    //OnRequestSuit();
 }
 function RefreshTable(state){
+    tableState = state;
     var westSeat = seat+1;
     var northSeat = seat+2;
     var eastSeat = seat+3;
@@ -21,17 +15,26 @@ function RefreshTable(state){
     if (northSeat>3) northSeat -=4;
     if (eastSeat>3) eastSeat -=4;
 
+    var s = 0;
+    var okToShowPlay = false;
+    for (s = 0 ; s<=3 ; s++){
+        if (state.seats[s].played !== null){okToShowPlay = true;}
+    }
+
+    if (okToShowPlay){$( "#tblPlay" ).show();}else{$( "#tblPlay" ).hide();}
+
+
     //South Seat
-    RefreshPlayerDisplay(state.seats[seat],'playerNameSouth');
+    RefreshPlayerDisplay(seat,'playerNameSouth', 'playedCardSouth');
     //West Seat
     DealHiddenHand('divHandWest', state.seats[westSeat].hand);
-    RefreshPlayerDisplay(state.seats[westSeat],'playerNameWest');
+    RefreshPlayerDisplay(westSeat,'playerNameWest','playedCardWest');
     //North Seat
     DealHiddenHand('divHandNorth', state.seats[northSeat].hand);
-    RefreshPlayerDisplay(state.seats[northSeat],'playerNameNorth');
+    RefreshPlayerDisplay(northSeat,'playerNameNorth', 'playedCardNorth');
     //East Seat
     DealHiddenHand('divHandEast', state.seats[eastSeat].hand);
-    RefreshPlayerDisplay(state.seats[eastSeat],'playerNameEast');
+    RefreshPlayerDisplay(eastSeat,'playerNameEast', 'playedCardEast');
 
     RefreshKitty(state.kitty_cnt);
 
@@ -43,10 +46,11 @@ function RefreshTable(state){
 }
 function OnRequestBid(req_bid){
     SetBidOptions(req_bid.min, req_bid.max);
-    $('#divMessage').html("Your turn to bid")
+    $('#divMessage').html("Your turn to bid");
      $('#divBid').show();
 }
 function OnRequestSuit(data){
+    $('#divMessage').html();
     $( "#divSelectSuit" ).show();
 }
 
@@ -82,39 +86,32 @@ function DealHiddenHand(handName, ct){
     }
 }
 
-function RefreshPlayerDisplay(seat, displayName){
+function RefreshPlayerDisplay(seatNum, displayName, playCardName){
+    var seat = tableState.seats[seatNum];
     var str = '';
     if (seat.name !== null) str += seat.name;
 
-    if (seat.bid === 0){
+    if (seat.bid === 0 && tableState.trump === null){
         str += " &bull; Pass";
     }
     else if (seat.bid !== null && seat.bid > 0){
         str += " &bull; " + seat.bid;
     }
-    if (tableState.trump === 'S') str += " &bull; &spadesuit;"
-    else if (tableState.trump === 'D') str += " &bull; &diams;"
-    else if (tableState.trump === 'C') str += " &bull; &clubsuit;"
-    else if (tableState.trump === 'H') str += " &bull; &heartsuit;"
+
+    if (tableState.bidder === seatNum){
+        if (tableState.trump === 'S') str += " &bull; &spadesuit;"
+        else if (tableState.trump === 'D') str += " &bull; &diams;"
+        else if (tableState.trump === 'C') str += " &bull; &clubsuit;"
+        else if (tableState.trump === 'H') str += " &bull; &heartsuit;"
+    }
+
+
+
+    SetPlayCard(seat.played, playCardName);
 
     $( "#"+displayName ).html(str);
 }
 
-function SetBidTurn(playerID, minBid) {
-    if (playerID === -1) {
-        $( "#divBid" ).hide();
-    }else if (playerID === 1){
-        $( "#divBid" ).show();
-        $( "#bidStatusMessage" ).html("It's your turn to bid...");
-        SetBidOptions(minBid);
-    }else
-    {
-        $( "#divBid" ).show();
-        $( "#bidStatusMessage" ).html("Waiting for Player " + playerID + " to bid...");
-    }
-
-    SetBidOptions(5);
-}
 function SetBidOptions(minBid, maxBid){
     var options = document.getElementById("bidOptions");
     options.innerHTML="";
@@ -172,6 +169,7 @@ function SendBid(bid){
 
     $( "#divBid" ).hide();
     socket.emit('bid', bid);
+    $('#divMessage').html("");
 }
 function OnSelectSuit(suit){
     socket.emit('suit', suit);
@@ -181,24 +179,40 @@ function OnSelectSuit(suit){
 function AddCardToHand(card, hand){
     var container = document.getElementById(hand);
     var img = document.createElement("img");
-    img.setAttribute("src", "../static/cards/" + card + ".svg")
-    img.setAttribute("class", "pcard")
+    img.setAttribute("src", "../static/cards/" + card + ".svg");
+    img.setAttribute("class", "pcard");
+    img.setAttribute("id", "card_" + card);
     img.onclick = function(){OnCardClick(card, hand);};
     container.appendChild(img);
 }
 function OnCardClick(theCard, hand){
     if (hand==='divHandSouth'){
-        socket.emit('play', theCard);
+        if (PlayableCards.includes(theCard)){
+            socket.emit('play', theCard);
+        }
     }
+}
+var PlayableCards = [];
+function OnRequestPlay(data){
+    var i = 0;
+    for (i = 0 ; i < data.length ; i++){
+        var img = document.getElementById("card_" + data[i]);
+        img.classList.add("playableCard");
+    }
+    PlayableCards = data;
 }
 
 function SetPlayCard(card, elem){
     var container = document.getElementById(elem);
-    var img = document.createElement("img");
-    img.setAttribute("src", "../static/cards/" + card + ".svg")
-    img.setAttribute("class", "pcard")
     container.innerHTML = "";
-    container.appendChild(img);
+    if (card !== null){
+        var img = document.createElement("img");
+        img.setAttribute("src", "../static/cards/" + card + ".svg")
+        img.setAttribute("class", "pcard")
+        container.innerHTML = "";
+        container.appendChild(img);
+    }
+
 }
 
 function HandleChatClick(){
